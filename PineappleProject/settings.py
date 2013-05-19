1b52701bd6aea4c03143eefaec4e2e1ace23182e
Peter Newhook <peter.newhook@gmail.com>
2013-05-18 23:05:42 -0400

    using azure

--- PineappleProject/settings.py
+++ PineappleProject/settings.py
@@ -72,7 +72,7 @@ STATIC_ROOT = root('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://graffiticatalog.blob.core.windows.net/pineapple/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

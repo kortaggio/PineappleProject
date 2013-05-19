1d5cd2683e6d839092328e400f471def4fd99f54
Peter Newhook <peter.newhook@gmail.com>
2013-05-18 20:05:01 -0400

    app added

--- PineappleProject/settings.py
+++ PineappleProject/settings.py
@@ -1,4 +1,11 @@
+from os.path import join, abspath, dirname
 # Django settings for PineappleProject project.
+
+# Variables to reference locations in the project structure
+
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+PROJECT_ROOT = here("..")
+root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -72,6 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    root('static')
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    root('templates'),
 )
 
 INSTALLED_APPS = (
@@ -124,6 +133,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'webapp',
 )
 
 # A sample logging configuration. The only tangible logging

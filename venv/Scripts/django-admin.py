51b0459c296b9e0c659d2a53ef43e63509ccdb46
Kortaggio <contact@kortaggio.com>
2014-07-18 22:57:31 -0400

    Update to static splash page files

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Peter\Documents\Visual Studio 2012\Projects\PineappleProject\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
